25e5d9367c20c18c6b3bb2882971650ebfe5b501
SkrNLALM <cakmakskr@gmail.com>
2023-02-11 21:48:16 +0100

    some updates

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -5,7 +5,7 @@ public class Task
 
     public static void main(String[] args) {
 
-
+        //try something
         System.out.println("Hello World");
         System.out.println("Hello World2");
 
